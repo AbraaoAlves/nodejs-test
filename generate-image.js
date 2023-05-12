8c8df9fd01f78bc498f6b9aaa3f1e982a2b11254
AbraaoAlves <abraao.teodosio@gmail.com>
2023-05-12 17:10:50 -0300

    implement generate image

--- generate-image.js
+++ generate-image.js
@@ -1,23 +1,20 @@
-import { createCanvas } from 'canvas';
+import { createCanvas } from 'canvas'
+import {writeFileSync} from 'node:fs';
+import path from 'path';
+const __dirname = path.resolve();
 
-import fs from 'fs';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+export default function generate_image(textContext) {
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+  const canvas = createCanvas(800, 400)
+  const context = canvas.getContext('2d')
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, 800, 400)
+  context.font = '30px Impact'
+  context.fillStyle = '#fff'
+  context.fillText(textContext, 50, 100)
+  const buffer = canvas.toBuffer('image/png')
+  // Save the image to a file
+  writeFileSync(__dirname + '/image.png', buffer)
 
-export default (textContent) => {
-
-    const canvas = createCanvas(200, 200)
-    const context = canvas.getContext('2d')
-    context.fillStyle = '#000'
-    context.fillRect(0, 0, 200, 200)
-    context.font = '30px Impact'
-    context.fillStyle = '#fff'
-    context.fillText('hello worlds', 50, 100)
-    
-    const buffer = canvas.toBuffer('image/png')
-    
-    // Save the image to a file
-    fs.writeFileSync(__dirname + '/image.png', buffer);
 }
+
